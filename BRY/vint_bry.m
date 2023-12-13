bf09274c45c7da0b761ee4fbc7ba026b0bd798d0
Jeroen Molemaker <nmolem@atmos.ucla.edu>
2023-12-13 14:20:13 -0800

    changes to BRY

--- BRY/vint_bry.m
+++ BRY/vint_bry.m
@@ -3,8 +3,9 @@
   % We rely on a strict naming convection for varnames:
   % u_south,v_south,temp_south, etc, etc
 
-  gname = '/paracas/nmolem/GREEN/green_grd.nc';
-  fname = '/paracas/nmolem/GREEN/green_bry.20140821002229.nc';
+  gname = '/zulu/nmolem/GWAKE/gwake_grd.nc';
+  fname = '/zulu/nmolem/GWAKE/gwake_bry.201408.nc';
+
   oname = [fname(1:end-3) '_int.nc'];
 
   ts= 6;
@@ -29,10 +30,12 @@
     tic
     vname = inf.Variables(i).Name
     f = ncread(fname,vname);
+    lname  =  ncreadatt(fname,vname,'long_name');
+    units  =  ncreadatt(fname,vname,'units');
+
 
     if ndims(f)<3 % surface var or time
-      if ndims(f)<2 % time
-	      F always has 2 or more dims
+      if size(f,2)==1 % time
         nccreate(oname,vname,'dimensions',{'time',0},'datatype','double');
       else
         [h_bnd,bdimx] = get_hbnd(vname,h);
@@ -40,16 +43,23 @@
         nccreate(oname,vname,'dimensions',{bdimx,nx,'time',0},'datatype','double');
       end
       ncwrite(oname,vname,f)
+      ncwriteatt(oname,vname,'long_name',lname)
+      ncwriteatt(oname,vname,'units',units)
       continue
     end
     nx = size(f,1);
     nt = size(f,3);
 
     % Get appropriate topography at the boundary
-%   [h_bnd,bdimx] = get_hbnd(vname,h);
-    h_bnd = h_bnd';
-    z_bnd = zlevs3(h_bnd,0*h_bnd,ts,tb,hc,nz,'r','new2008')';
-    zo_bnd = zlevs3(h_bnd,0*h_bnd,tso,tbo,hco,nzo,'r','new2008')';
+    [h_bnd,bdimx] = get_hbnd(vname,h);
+    z_bnd  = zlevs3(h_bnd,0*h_bnd,ts,tb,hc,nz,'r','new2008');
+    zo_bnd = zlevs3(h_bnd,0*h_bnd,tso,tbo,hco,nzo,'r','new2008');
+    z_bnd  = squeeze(z_bnd)';
+    zo_bnd = squeeze(zo_bnd)';
+
+    nccreate(oname,vname,'dimensions',{bdimx,nx,bdimz,nzo,'time',0},'datatype','double');
+    ncwriteatt(oname,vname,'long_name',lname)
+    ncwriteatt(oname,vname,'units',units)
 
     toc
 
@@ -60,7 +70,6 @@
       fint = interp1(z_bnd(i,:),squeeze(f(i,:,:)),zo_bnd(i,:),'linear','extrap');
       fo(i,:,:) = fint;
     end
-    nccreate(oname,vname,'dimensions',{bdimx,nx,bdimz,nzo,'time',0},'datatype','double');
     ncwrite(oname,vname,fo)
     toc
   end
